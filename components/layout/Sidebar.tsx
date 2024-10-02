954002f510b4b28455312d867dad7389d0c96e8c
Matteo Luca <info@matthewjohn.ch>
2024-10-02 16:01:38 +0200

    updated sidebar

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,50 +1,59 @@
-import { BsBellFill, BsHouseFill } from "react-icons/bs";
-import { FaUser } from "react-icons/fa";
-import SidebarItem from "./SidebarItem";
-import SidebarLogo from "./SidebarLogo";
-import { BiLogOut } from "react-icons/bi";
-import SidebarTweetButton from "./SidebarTweetButton";
+import { signOut } from 'next-auth/react';
+import { BiLogOut } from 'react-icons/bi';
+import { BsHouseFill, BsBellFill } from 'react-icons/bs';
+import { FaUser } from 'react-icons/fa';
+
+import useCurrentUser from '@/hooks/useCurrentUser';
+
+import SidebarItem from './SidebarItem';
+import SidebarLogo from './SidebarLogo';
+import SidebarTweetButton from './SidebarTweetButton';
+
 const Sidebar = () => {
-   
-   const items =[
+  const { data: currentUser } = useCurrentUser();
+
+  const items = [
     {
-        label: 'Home',
-        href: '/',
-        icon: BsHouseFill
+      icon: BsHouseFill,
+      label: 'Home',
+      href: '/',
     },
     {
-        label: 'Notifiche',
-        href: '/notifications',
-        icon: BsBellFill
+      icon: BsBellFill,
+      label: 'Notifications',
+      href: '/notifications',
+      auth: true,
+      alert: currentUser?.hasNotification
     },
     {
-        label: 'Profilo',
-        href: '/users/123',
-        icon: FaUser
-    }
-   ];
-    return ( 
-        <div className="col-span-1 h-full pr-4 md:pr-6">
-            <div className="flex flex-col items-end">
-                <div className="space-y-2 lg:2-[230px]">
-                <SidebarLogo />
-                {items.map((item) => (
+      icon: FaUser,
+      label: 'Profile',
+      href: `/users/${currentUser?.id}`,
+      auth: true,
+    },
+  ]
 
-                    <SidebarItem
-                    key= {item.href}
-                    href= {item.href}
-                    label = {item.label}
-                    icon = {item.icon}
-
-                    />
-                ) )}
-                <SidebarItem onClick={() => {}} icon={BiLogOut} label="Logout" />
-                    <SidebarTweetButton />
-                </div>
-
-            </div>
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+        <div className="flex flex-col items-end">
+          <div className="space-y-2 lg:w-[230px]">
+            <SidebarLogo />
+            {items.map((item) => (
+              <SidebarItem
+                key={item.href}
+                alert={item.alert}
+                auth={item.auth}
+                href={item.href} 
+                icon={item.icon} 
+                label={item.label}
+              />
+            ))}
+            {currentUser && <SidebarItem onClick={() => signOut()} icon={BiLogOut} label="Logout" />}
+            <SidebarTweetButton />
+          </div>
         </div>
-     );
-}
- 
+      </div>
+  )
+};
+
 export default Sidebar;
